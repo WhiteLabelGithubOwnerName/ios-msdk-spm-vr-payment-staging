e0c48a0edeb7366309b06e7c761e5c293fcc9fe3
wallee-deployment-user <technical-systems@wallee.com>
2026-06-12 11:37:22 +0200

    2.0.0

--- Package.swift
+++ Package.swift
@@ -6,12 +6,14 @@ let package = Package(
     platforms: [.iOS("12.4")],
     products: [
         .library(name: "ReactBrownfield", targets: ["ReactBrownfield"]),
+        .library(name: "ThreeDS_SDK", targets: ["ThreeDS_SDK"]),
         .library(name: "TwintSDK", targets: ["TwintSDK"]),
         .library(name: "VRPaymentSdk", targets: ["VRPaymentSdk"]),
         .library(name: "hermes", targets: ["hermes"])
     ],
     targets: [
         .binaryTarget(name: "ReactBrownfield", path: "./ReactBrownfield.xcframework"),
+        .binaryTarget(name: "ThreeDS_SDK", path: "./ThreeDS_SDK.xcframework"),
         .binaryTarget(name: "TwintSDK", path: "./TwintSDK.xcframework"),
         .binaryTarget(name: "VRPaymentSdk", path: "./VRPaymentSdk.xcframework"),
         .binaryTarget(name: "hermes", path: "./hermes.xcframework")
